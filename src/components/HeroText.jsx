b547a1af3abd0bb73d7e65b97c463fc2e28e8251
Utkarsh Sharma <utkarshdev2411@gmail.com>
2025-04-27 03:18:52 +0530

    Update HeroText and About sections: change name to Utkarsh Sharma and update key phrases

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -2,7 +2,8 @@ import { FlipWords } from "./FlipWords";
 import { motion } from "motion/react";
 
 const HeroText = () => {
-  const words = ["Secure", "Modern", "Scalable"];
+  const words = ["GenAI", "MERN", "FullStack", "Frontend", "Backend"];
+
   const variants = {
     hidden: { opacity: 0, x: -50 },
     visible: { opacity: 1, x: 0 },
@@ -18,7 +19,7 @@ const HeroText = () => {
           animate="visible"
           transition={{ delay: 1 }}
         >
-          Hi I'm Ali
+          Hi I&#39;m Utkarsh Sharma
         </motion.h1>
         <div className="flex flex-col items-start">
           <motion.p
@@ -28,7 +29,7 @@ const HeroText = () => {
             animate="visible"
             transition={{ delay: 1.2 }}
           >
-            A Developer <br /> Dedicated to Crafting
+            A Developer <br /> Dedicated to Building
           </motion.p>
           <motion.div
             variants={variants}
@@ -61,8 +62,8 @@ const HeroText = () => {
           animate="visible"
           transition={{ delay: 1 }}
         >
-          Hi,I'm Ali
-        </motion.p>
+          Hi I&#39;m Utkarsh Sharma
+          </motion.p>
         <div>
           <motion.p
             className="text-5xl font-black text-neutral-300"
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
             className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
           />
           <div className="z-10">
-            <p className="headtext">Hi, I&apos;m Ali Sanati</p>
+            <p className="headtext">Hi, I&apos;m Utkarsh Sharma</p>
             <p className="subtext">
               Over the last 4 years, I developed my frontend and backend dev
               skills to deliver dynamic and software and web applications.
