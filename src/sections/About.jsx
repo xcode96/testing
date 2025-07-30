1d30ad56e034a6069988a4cd053ae297a00ac639
Mani Bharathi P <charuxdq@gmail.com>
2025-07-30 23:31:45 +0530

    About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -17,10 +17,9 @@ const About = () => {
             className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
           />
           <div className="z-10">
-            <p className="headtext">Hi, I'm Ali Sanati</p>
+            <p className="headtext">Hi, I'm Mani Bharathi</p>
             <p className="subtext">
-              Over the last 4 years, I developed my frontend and backend dev
-              skills to deliver dynamic and software and web applications.
+              As a principled builder, I see that cybersecurity and policy are too often discussed in isolation from the human experience.As a systems-level thinker, I believe it's time to reframe the conversation — placing resilience, empathy, and ethics at its core..
             </p>
           </div>
           <div className="absolute inset-x-0 pointer-evets-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo" />
