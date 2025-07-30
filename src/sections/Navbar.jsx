d36182f5ecb1a9caedca70e5320828b29f92fd0c
Mani Bharathi P <charuxdq@gmail.com>
2025-07-30 23:47:58 +0530

    Navbar.jsx

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
             href="/"
             className="text-xl font-bold transition-colors text-neutral-400 hover:text-white"
           >
-            Ali
+            Xcode96
           </a>
           <button
             onClick={() => setIsOpen(!isOpen)}
